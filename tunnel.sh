caa4f614c5e11519fa26a88f85423d158c241a2d
bgrotten <ben.grottenegg@gmail.com>
2021-05-28 15:26:19 +0200

    tunnel_srcipt adapted

--- tunnel.sh
+++ tunnel.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh -f -i ~/.ssh/id_rsa benno_grottenegg@3.215.176.179 -L 3306:production-us.ca2tik3n0kik.us-east-1.rds.amazonaws.com:3306 -N
+ssh -f -i ~/.ssh/id_rsa benno_grottenegg@<ip-address> -L port:link:port -N
